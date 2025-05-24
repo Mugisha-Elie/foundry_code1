69b302b4b616a73380bd608f7c8289e8c221393b
Mugisha Elie <iradukunda2k24@gmail.com>
2025-05-24 12:36:07 +0200

    Add common features

---
+++ OurProject1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int choice;
+        do
+        {
+            Console.WriteLine("\nChoose an option:");
+            Console.WriteLine("1: Check if a number is Even or Odd");
+            Console.WriteLine("2: Check if a number is Positive or Negative");
+            Console.WriteLine("3: Exit");
+            Console.Write("Enter your choice (1, 2 or 3): ");
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    
+
+                case 2:
+                    
+
+                case 3:
+                    Console.WriteLine("Exiting the program. Goodbye!");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please enter 1, 2, or 3.");
+                    break;
+            }
+
+        } while (choice != 3);
+    }
+}
