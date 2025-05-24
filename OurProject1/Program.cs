d07418bdb72adb740a73f72ca921ba68d4cb9c98
mutabazi001 <mutabaziallan320@gmail.com>
2025-05-24 13:04:22 +1100

    odd and even

--- OurProject1/Program.cs
+++ OurProject1/Program.cs
@@ -18,6 +18,13 @@ class Program
             {
                 case 1:
                     
+                    Console.Write("Enter a number: ");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+                    if (num1 % 2 == 0)
+                        Console.WriteLine($"{num1} is Even.");
+                    else
+                        Console.WriteLine($"{num1} is Odd.");
+                    break;
 
                 case 2:
                     
